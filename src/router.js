de7cd0770aff79d3db3b5848dc92568030cd6179
liangqingtian <liangqingtian@bilibili.com>
2021-02-22 17:10:01 +0800

    add named chunks

--- src/router.js
+++ src/router.js
@@ -3,13 +3,17 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
+const Home = () => import(/* webpackChunkName: "home" */ './components/Home.vue');
+const Profile = () => import(/* webpackChunkName: "profile" */ './components/Profile.vue');
+const NotFound = () => import(/* webpackChunkName: "404" */ './components/404.vue');
+
 export default function createRouter() {
   return new VueRouter({
     mode: 'history',
     routes: [
-      { path: '/home', component: () => import('./components/Home.vue') },
-      { path: '/profile', component: () => import('./components/Profile.vue') },
-      { path: '*', component: () => import('./components/404.vue') },
+      { path: '/home', component: Home },
+      { path: '/profile', component: Profile },
+      { path: '*', component: NotFound },
     ]
   })
 }
