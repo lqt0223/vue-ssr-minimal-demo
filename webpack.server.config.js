1e5442bca23942f85d6f9dadf9cc9d519c67417d
liangqingtian <liangqingtian@bilibili.com>
2021-02-24 17:02:41 +0800

    adds externals (for server app packing, library codes are used at runtime from node_modules instead of packed in to bundle)

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -2,6 +2,7 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.config.js');
 const path = require('path');
 const VueSSRServerPlugin = require('vue-server-renderer/server-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = merge(baseConfig, {
   entry: './src/entry-server.js',
@@ -9,6 +10,9 @@ module.exports = merge(baseConfig, {
   output: {
     libraryTarget: 'commonjs2'
   },
+  externals: nodeExternals({
+    allowlist: /\.css$/
+  }),
   module: {
     rules: [
       {
